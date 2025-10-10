6e6de266c825666100e72e291c929146900cdf08
pooja20326 <poojaselvaraj2611@gmail.com>
2025-10-10 12:17:33 +0530

    initial commit

---
+++ String1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class String1 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        int c=A.length()+B.length();
+        System.out.println(c);
+    }
+}
