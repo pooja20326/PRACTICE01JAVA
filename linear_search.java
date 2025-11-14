16084c59c182eba51a8cda8cbd824636dc9f32fb
pooja20326 <poojaselvaraj2611@gmail.com>
2025-11-14 21:33:09 +0530

    after changing

--- linear_search.java
+++ linear_search.java
@@ -3,25 +3,24 @@ public class linear_search
 {
     public static void main(String[] args) 
     {
-        Scanner sc =new scanner(System.in);
-        System.out.println("Enter the key");
-        int n=sc.nextInt(system.in);
-        int[] arr=new arr[5];
+        Scanner sc =new Scanner(System.in);
+       
+        int[] arr=new int[5];
         System.out.println("Enter the array element");
         for(int  i=0;i<arr.length-1;i++)
         {
            arr[i]=sc.nextInt();
         }
+         System.out.println("Enter the key");
+        int n=sc.nextInt();
         for(int i=0;i<n;i++)
         {
             if(arr[i]==n)
             {
-                system.out.println("The value is presented in :" + i + "index of the array");
+                System.out.println("The value is presented in :" + i + "index of the array");
             }
-            else
-            {
-                System.out.println(" the element not found");
-            }
+          
         }
-    }
+          System.out.println(" the element not found");
+        }
 }
