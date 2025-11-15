532c6ab8fe84a64fe1e4bc63fefe7a0bc6792f6e
pooja20326 <poojaselvaraj2611@gmail.com>
2025-11-15 11:45:31 +0530

    initial commit

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray {
+public static void main(String[] args) {
+    int[] arr={12,13,45,67,89,90,55};
+      System.out.println(" The reversed array is");
+    for(int i=arr.length-1;i>=0;i--)
+    {
+      System.out.print(arr[i]+" ");
+    }
+}
+    
+}
