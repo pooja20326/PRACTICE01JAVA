04e474c7f0d3529ef1285ecdc80de9d045ea3b31
pooja20326 <poojaselvaraj2611@gmail.com>
2025-12-27 16:20:08 +0530

    method overriding

---
+++ AnimalSound.java
@@ -0,0 +1,22 @@
+class Animal
+{
+    void Sound()
+    {
+        System.out.println("Animal make sound");
+    }
+}
+class dog extends Animal
+{
+    void Sound()
+    {
+        System.out.println("Dog barking");
+    }
+}
+public class AnimalSound {
+    public static void main(String[] args) {
+        dog d=new Dog();
+        
+    }
+
+    
+}
