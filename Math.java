2e770d81b3f680ce092a70ea82092ba3d6fbc07e
pooja20326 <poojaselvaraj2611@gmail.com>
2025-12-27 16:24:45 +0530

    method overriding-practice

---
+++ Math.java
@@ -0,0 +1,23 @@
+class Shape
+{
+    void Draw()
+    {
+        System.out.println("Drawing Shape");
+    }
+}
+class Circle extends Shape
+{
+    void Draw()
+    {
+        System.out.println("Drawing Circle");
+    }
+}
+public class Math {
+    public static void main(String[] args) 
+    {
+        Shape d=new Circle();
+        d.Draw();   
+    }
+
+   
+}
