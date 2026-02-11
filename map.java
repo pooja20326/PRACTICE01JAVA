f2ace30610f8e09f8c75f6142027f6dd69fd400e
pooja20326 <poojaselvaraj2611@gmail.com>
2026-02-11 20:27:18 +0530

    Basic operations of MAP---COLLECTION(MAP_BASIC)

---
+++ map.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class map 
+{
+public static void main(String[] args) 
+{
+    HashMap<Integer, String> maps=new HashMap<>();
+    maps.put(1,"pooja");
+    maps.put(2,"eniyan");
+    maps.put(3,"Vasuki");
+    maps.put(4,"Gopika");
+    maps.put(5,"Dhasarathi");
+    maps.put(6,"Nandhini");
+    maps.put(7,"Samu");
+    System.out.println(maps.get(4));
+    System.out.println(maps.remove(6));
+    System.out.println(maps.containsKey(5));
+    System.out.println(maps.containsValue("pooja"));
+    System.out.println(maps.size());
+
+}
+}
